bbb2617b1321fec9752edad7cf25a7b1f94c96cc
Axmadjon Isaqov <131191067+axiyisakov@users.noreply.github.com>
2024-08-05 15:26:22 +0500

    first commit

---
+++ bin/dart_lessons.dart
@@ -0,0 +1,7 @@
+//axiy.dev
+import 'package:dart_lessons/dart_lessons.dart' as dart_lessons;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_lessons.calculate()}!');
+  print("Hello world");
+}
